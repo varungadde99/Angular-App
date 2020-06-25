533d41a18a1a79974b8412f8e2e7e9609c677e64
Varun Gadde <varungadde99@gmail.com>
2020-06-25 20:00:31 +0530

    'Mat-Grid-List  and Card Component'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,14 @@ import { NgModule } from "@angular/core";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { FlexLayoutModule } from "@angular/flex-layout";
-
 import { AppComponent } from "./app.component";
-import "hammerjs";
 import { MenuComponent } from "./menu/menu.component";
 import { MatListModule } from "@angular/material/list";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+
+import "hammerjs";
 @NgModule({
   declarations: [AppComponent, MenuComponent],
   imports: [
@@ -16,6 +19,9 @@ import { MatListModule } from "@angular/material/list";
     MatToolbarModule,
     FlexLayoutModule,
     MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
