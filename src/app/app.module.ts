65f82262c1eda6947fdaf1d3e0f7f3a02c6cd284
Varun Gadde <varungadde99@gmail.com>
2020-07-01 11:34:45 +0530

    'App Routing'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,10 @@ import { DishdetailComponent } from "./dishdetail/dishdetail.component";
 import { DishService } from "./services/dish.service";
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
+import { HomeComponent } from "./home/home.component";
+import { AboutComponent } from "./about/about.component";
+import { ContactComponent } from "./contact/contact.component";
+import { AppRoutingModule } from "./app-routing/app-routing.module";
 
 @NgModule({
   declarations: [
@@ -22,10 +26,14 @@ import { FooterComponent } from "./footer/footer.component";
     DishdetailComponent,
     HeaderComponent,
     FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    AppRoutingModule,
     MatToolbarModule,
     FlexLayoutModule,
     MatListModule,
