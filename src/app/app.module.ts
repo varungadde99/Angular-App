6a30628704036d2e8ac7f864ef67ec1e231cd783
Varun Gadde <varungadde99@gmail.com>
2020-07-01 19:33:20 +0530

    'Single Page App - SPA'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { HomeComponent } from "./home/home.component";
 import { AboutComponent } from "./about/about.component";
 import { ContactComponent } from "./contact/contact.component";
 import { AppRoutingModule } from "./app-routing/app-routing.module";
+import { PromotionService } from "./services/promotion.service";
 
 @NgModule({
   declarations: [
@@ -42,7 +43,7 @@ import { AppRoutingModule } from "./app-routing/app-routing.module";
     MatButtonModule,
   ],
   //All the services are treated as providers.
-  providers: [DishService],
+  providers: [DishService, PromotionService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/services/dish.service.ts
+++ src/app/services/dish.service.ts
@@ -1,16 +1,22 @@
-import { Injectable } from '@angular/core';
-import { Dish } from '../shared/dish';
-import { DISHES } from '../shared/dishes';
+import { Injectable } from "@angular/core";
+import { Dish } from "../shared/dish";
+import { DISHES } from "../shared/dishes";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root",
 })
 export class DishService {
-
-  constructor() { }
+  constructor() {}
 
   getDishes(): Dish[] {
     return DISHES;
-}
+  }
 
+  getDish(id: string): Dish {
+    return DISHES.filter((dish) => dish.id === id)[0];
+  }
+
+  getFeaturedDish(): Dish {
+    return DISHES.filter((dish) => dish.featured)[0];
+  }
 }
